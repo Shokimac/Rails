fd18c5d0ecba5f3348304244b2976a20878f19f7
Shoki Yokomizo <shokimac@Shokis-mac.local>
2020-05-27 14:31:56 +0900

    chapter8_finish

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'a6ac99476686ec09c15574050e557970c684f4e0bb5123542a337c75ec609e95d37eadffd5658c7fc339fa01fe89097a463cacdde059993e90f8189e8635a5dc'
